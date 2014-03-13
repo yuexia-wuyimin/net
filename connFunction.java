609fad596984b8cfbc8f915869ff80b009727d08
unknown <wuyimin@wuyimin-yuexia.(none)>
2014-03-14 00:53:14 +0800

    my first

---
+++ connFunction.java
@@ -0,0 +1,33 @@
+package com.Yim.ConnUtil;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import com.Yim.json.WriteJson;
+
+public class connFunction {
+	public String connUseHttpClient(List<?> wantToSend,String strutsAction){
+		HttpClient hc= new HttpClient();
+		
+		String wantToSentJson;
+		WriteJson writeToJson=new WriteJson();
+		wantToSentJson = writeToJson.getJsonData(wantToSend);
+		System.out.println(wantToSentJson);
+		
+		PostMethod postMethod = new PostMethod(com.automan.basic.ConnUtility.Server+"/androidaction/"+strutsAction+".action");
+		postMethod.setParameter(strutsAction,wantToSentJson);
+		String returnFromStruts="";
+		try{
+			hc.executeMethod(postMethod);
+			returnFromStruts=postMethod.getResponseBodyAsString();
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			postMethod.releaseConnection();
+		}
+		return returnFromStruts;	
+	}
+}
